4442eec47726d70ae47ccafff2ee155d0ed9fb9a
cse-f <verify.moneymind@gmail.com>
2024-11-18 23:01:15 +0530

    Create media_query.js

---
+++ html/media_query.js
@@ -0,0 +1,26 @@
+//media queries
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animation Example</title>
+    <style>
+      .cls {
+        background-color: red;
+        height: 100px;
+        width: 100px;
+      }
+      @media (max-width:600px){
+        .cls{
+          background-color: blue;
+        }
+        
+      }
+    </style>
+</head>
+<body>
+    <div class="cls"></div>
+</body>
+</html>
