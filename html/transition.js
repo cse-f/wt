931ca4d44d2e956a86a00f626e1087e4d235d0ee
cse-f <verify.moneymind@gmail.com>
2024-11-18 22:41:50 +0530

    Create transition.js

---
+++ html/transition.js
@@ -0,0 +1,27 @@
+//trnasition
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>transition</title>
+    <style>
+      .cls{
+        background-color: red;
+        height: 100px;
+        width: 100px;
+        transition:all 1s;
+      }
+      
+        .cls:hover {
+            width: 150px;
+            height: 150px;
+        }
+    </style>
+</head>
+<body >
+    <div class="cls">
+    </div>
+</body>
+</html>
