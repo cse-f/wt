fa76716bfc5b312cd9fa6b603075dd1e00c81419
cse-f <verify.moneymind@gmail.com>
2024-11-18 21:25:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 HTML-registration Form------------>     curl -k https://cse-f.github.io/wt/html/registration_form.js
 HTML-various ways of adding styles>     curl -k https://cse-f.github.io/wt/html/styles.js
+HTML-selectors-------------------->     curl -k https://cse-f.github.io/wt/html/selectors.js
+
 
 
 JS-fibonacci series----------->     curl -k https://cse-f.github.io/wt/js/fib.js
@@ -8,11 +10,14 @@ JS-factorial------------------>     curl -k https://cse-f.github.io/wt/js/fact.j
 JS-regular Expressions-------->     curl -k https://cse-f.github.io/wt/js/regex.js
 JS-calculator----------------->     curl -k https://cse-f.github.io/wt/js/calci.js
 
+
   
 JQUERY-EVENTS(mouseUP,Down,enter,leave,focus)---->        curl -k https://cse-f.github.io/wt/jq/1.js
 JQUERY-ACTIONS(show,hide,t)---->       curl -k https://cse-f.github.io/wt/jq/2.js
 JQUERY-SLIDEMETHODS------------>       curl -k https://cse-f.github.io/wt/jq/4.js
 JQUERY-FADEMETHODS------------->       curl -k https://cse-f.github.io/wt/jq/3.js
+
+
 
 JDBC-SELECT--------------------->         curl -k https://cse-f.github.io/wt/jdbc/1.js
 JDBC-INSERT,Update,Delete------->         curl -k https://cse-f.github.io/wt/jdbc/2.js
