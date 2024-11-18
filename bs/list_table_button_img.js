c058f9b263daf173274b180b04c8202905e9d6c8
cse-f <verify.moneymind@gmail.com>
2024-11-18 23:59:11 +0530

    Create list_table_button_img.js

---
+++ bs/list_table_button_img.js
@@ -0,0 +1,73 @@
+//bootstrap elements
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Grid Example</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+   <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <ul class="list-group">
+          <li class="list-group-item">first</li>
+          <li class="list-group-item">second</li>
+          <li class="list-group-item">third</li>
+          </ul>
+      </div>
+      <div class="col-md-6">
+        <ol class="list-group list-group-numbered">
+            <li class="list-group-item">First item</li>
+            <li class="list-group-item">Second item</li>
+            <li class="list-group-item">Third item</li>
+        </ol>
+    </div>
+    </div>
+    <div class="m-5">
+    <table class="table table-striped table-hover">
+      <tr>
+        <th>first</th>
+        <th>second</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+      </tr>
+    </table>
+    </div>
+
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-success">Success</button>
+    <button class="btn btn-danger">Danger</button>
+    <button class="btn btn-warning">Warning</button>
+    <button class="btn btn-info">Info</button>
+
+    <div class="row mt-5 mb-5">
+    <div class="col-sm-6">
+    <img src="https://via.placeholder.com/400x300" alt="Placeholder Image" class="img-fluid rounded">
+    </div>
+    <div class="col-sm-6">
+    <img src="https://via.placeholder.com/400x300" alt="Placeholder Image" class="img-fluid rounded-circle">
+    </div>
+
+    </div>
+
+
+    </div>
+    
+
+        
+</body>
+</html>
