5491c0ef3327c80e307c363de721a245cb514ffd
cse-f <verify.moneymind@gmail.com>
2024-11-18 23:31:31 +0530

    Create gridSystem.js

---
+++ bs/gridSystem.js
@@ -0,0 +1,24 @@
+//grid layout in bootstrap
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Grid Example</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 bg-primary col-sm-4 m-10" style="border: solid;">this is first</div>
+        <div class="col-md-6 bg-primary col-sm-8 m-10" style="border: solid;">this is second</div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 bg-primary col-lg-4 m-10" style="border: solid;">this is first</div>
+        <div class="col-md-6 bg-primary col-lg-8 m-10" style="border: solid;">this is second</div>
+      </div>
+    </div>
+        
+</body>
+</html>
