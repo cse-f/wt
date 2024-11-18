d97ac0aef2edca55c4868b7908fd0c6b32967caf
cse-f <verify.moneymind@gmail.com>
2024-11-19 02:10:38 +0530

    Update dateAndTime.js

--- jsp/dateAndTime.js
+++ jsp/dateAndTime.js
@@ -1,7 +1,3 @@
-//showing current date and Time
-
-<%@ page import="java.time.LocalDateTime" %>
-<%@ page import="java.time.format.DateTimeFormatter" %>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,19 +6,6 @@
     <title>Current Date and Time</title>
 </head>
 <body>
-    <h1>Current Date and Time</h1>
-    <p>
-        <%
-            // Get the current date and time
-            LocalDateTime currentDateTime = LocalDateTime.now();
-            
-            // Format the date and time
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            String formattedDateTime = currentDateTime.format(formatter);
-
-            // Display the formatted date and time
-            out.println("Current server date and time: " + formattedDateTime);
-        %>
-    </p>
+      <p>The current date and time is: <%= new java.util.Date() %></p>
 </body>
 </html>
